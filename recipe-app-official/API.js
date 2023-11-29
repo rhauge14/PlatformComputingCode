3a1cf84478975447352a535a6fbd838eae31f941
adonogh2 <127319957+adonogh2@users.noreply.github.com>
2023-11-29 16:09:51 -0500

    updating API

--- recipe-app-official/API.js
+++ recipe-app-official/API.js
@@ -1,1 +1,26 @@
+// SpoonacularService.js
 
+import axios from 'axios';
+
+const apiKey = 'YOUR_SPOONACULAR_API_KEY';
+
+export const getRecipeDetails = async (recipeId) => {
+  try {
+    const response = await axios.get(
+      `https://api.spoonacular.com/recipes/${recipeId}/information`,
+      {
+        params: {
+          apiKey: apiKey,
+          // Other parameters as needed
+        },
+      }
+    );
+
+    // Handle the response data
+    return response.data;
+  } catch (error) {
+    // Handle errors
+    console.error('Error fetching recipe details:', error);
+    throw error; // Optionally rethrow the error for the calling code to handle
+  }
+};
